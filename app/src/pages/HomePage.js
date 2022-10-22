0116148d3d4f2e2e3ac24bf4456beba029c09b8b
IslinaS <IslinaS@users.noreply.github.com>
2022-10-22 17:40:40 -0400

    changed style and add subtract button of homepage

--- app/src/pages/HomePage.js
+++ app/src/pages/HomePage.js
@@ -20,29 +20,36 @@ export const HomePage = ()=>{
         <div class="container">
           <div class="columns is-multiline is-mobile is-centered">
   
-            <div class = "column is-two-thirds">
-              <span class = "title is-4">
+            <center><div class = "column is-two-thirds">
+              <span class = "title is-6">
                 Where are you in the queue?
               </span>
-            </div>
+            </div></center>
   
             <div class = "column is-two-thirds">
-              <div class = "is-flex is-justify-content-center is-flex-direction-column">
+            <center><div class = "is-flex is-justify-content-center is-flex-direction-column">
                 <span class = "title ">{count}</span>
                 <span style={{fontSize:"1em"}}>people in front of me</span>
-              </div>
+              </div></center>
               
             </div>
   
             <div class = "column is-two-thirds m-5 pt-6">
               <div class = "is-flex is-justify-content-center ">
-                <button class = "button is-success is-large " onClick={()=>setCount(count+1)}>Click</button>
+                <div class = "field is-grouped">
+                  <p class = "control">
+                    <button class = "button is-success is-medium " style = {{ background: "#F6CF57"}} onClick={()=>setCount(count+1)}>+1</button>
+                  </p>
+                  <p class = "control">
+                    <button class = "button is-success is-medium " style = {{ background: "#F6CF57"}} onClick={()=>setCount(count>0?count-1:0)}>-1</button>
+                  </p>
+                </div>
               </div>
             </div>
   
             <div class = "column is-two-thirds">
               <div class = "is-flex is-justify-content-center ">
-                <button class = "button is-success is-large " onClick={handle_submit}>Submit</button>
+                <button class = "button is-success is-large " style = {{ background: "#F6CF57"}} onClick={handle_submit}>Submit</button>
               </div>
             </div>
   
